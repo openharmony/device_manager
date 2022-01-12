931b58ca7084a07cbe808cf2f4aa900201476452
puhui <puhui1@huawei.com>
2022-01-12 22:00:55 +0800

    提交DM框架与服务模块
    Signed-off-by: puhui <puhui1@huawei.com>

---
+++ services/devicemanagerservice/include/dependency/commonevent/event_manager_adapt.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OHOS_EVENT_MANAGER_ADAPT_H
+#define OHOS_EVENT_MANAGER_ADAPT_H
+
+#include <map>
+#include <mutex>
+
+#include "common_event.h"
+#include "common_event_data.h"
+#include "common_event_manager.h"
+#include "common_event_subscribe_info.h"
+#include "common_event_subscriber.h"
+#include "dm_log.h"
+#include "matching_skills.h"
+
+namespace OHOS {
+namespace DistributedHardware {
+
+using CommomEventCallback = void (*)(void);
+
+class DmCommonEventManager {
+public:
+    static DmCommonEventManager &GetInstance();
+
+public:
+    bool SubscribeServiceEvent(const std::string &event, CommomEventCallback callback);
+    bool UnsubscribeServiceEvent(const std::string &event);
+
+    class EventSubscriber : public EventFwk::CommonEventSubscriber {
+    public:
+        explicit EventSubscriber(const EventFwk::CommonEventSubscribeInfo &subscribeInfo)
+            : EventFwk::CommonEventSubscriber(subscribeInfo)
+        {
+        }
+
+        void OnReceiveEvent(const EventFwk::CommonEventData &data);
+        void addEventCallback(const std::string &event, CommomEventCallback callback);
+        void deleteEventCallback(const std::string &event);
+
+    private:
+        std::mutex callbackLock_;
+        std::map<std::string, CommomEventCallback> dmEventCallback_;
+    };
+
+private:
+    DmCommonEventManager() = default;
+    virtual ~DmCommonEventManager();
+    DmCommonEventManager(const DmCommonEventManager &) = delete;
+    DmCommonEventManager &operator=(const DmCommonEventManager &) = delete;
+    DmCommonEventManager(DmCommonEventManager &&) = delete;
+    DmCommonEventManager &operator=(DmCommonEventManager &&) = delete;
+
+private:
+    static void DealCallback(void);
+    static void InitCallbackThread(void);
+
+private:
+    static std::once_flag onceFlag_;
+    static std::list<CommomEventCallback> callbackQueue_;
+    static std::mutex callbackQueueMutex_;
+    static std::condition_variable notEmpty_;
+    std::map<std::string, std::shared_ptr<EventSubscriber>> dmEventSubscriber_;
+};
+} // namespace DistributedHardware
+} // namespace OHOS
+
+#endif // OHOS_EVENT_MANAGER_ADAPT_H
---
+++ services/devicemanagerservice/src/ipc/standard/ipc_server_stub.cpp
@@ -0,0 +1,187 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "ipc_server_stub.h"
+
+#include <algorithm>
+#include <thread>
+
+#include "device_manager_impl.h"
+#include "device_manager_service.h"
+#include "dm_constants.h"
+#include "dm_log.h"
+#include "if_system_ability_manager.h"
+#include "ipc_cmd_register.h"
+#include "ipc_skeleton.h"
+#include "ipc_types.h"
+#include "iservice_registry.h"
+#include "string_ex.h"
+#include "system_ability_definition.h"
+
+namespace OHOS {
+namespace DistributedHardware {
+IMPLEMENT_SINGLE_INSTANCE(IpcServerStub);
+
+const bool REGISTER_RESULT = SystemAbility::MakeAndRegisterAbility(&IpcServerStub::GetInstance());
+
+IpcServerStub::IpcServerStub() : SystemAbility(DISTRIBUTED_HARDWARE_DEVICEMANAGER_SA_ID, true)
+{
+    registerToService_ = false;
+    state_ = ServiceRunningState::STATE_NOT_START;
+}
+
+void IpcServerStub::OnStart()
+{
+    LOGI("IpcServerStub::OnStart start");
+    if (state_ == ServiceRunningState::STATE_RUNNING) {
+        LOGD("IpcServerStub has already started.");
+        return;
+    }
+    if (!Init()) {
+        LOGE("failed to init IpcServerStub");
+        return;
+    }
+    state_ = ServiceRunningState::STATE_RUNNING;
+}
+
+bool IpcServerStub::Init()
+{
+    LOGI("IpcServerStub::Init ready to init.");
+    if (!registerToService_) {
+        bool ret = Publish(this);
+        if (!ret) {
+            LOGE("IpcServerStub::Init Publish failed!");
+            return false;
+        }
+        registerToService_ = true;
+    }
+    std::thread{[] { DeviceManagerService::GetInstance().Init(); }}.detach();
+    return true;
+}
+
+void IpcServerStub::OnStop()
+{
+    LOGI("IpcServerStub::OnStop ready to stop service.");
+    state_ = ServiceRunningState::STATE_NOT_START;
+    registerToService_ = false;
+}
+
+int32_t IpcServerStub::OnRemoteRequest(uint32_t code, MessageParcel &data, MessageParcel &reply, MessageOption &option)
+{
+    LOGI("code = %d, flags= %d.", code, option.GetFlags());
+    int32_t ret = DM_OK;
+    ret = IpcCmdRegister::GetInstance().OnIpcCmd(code, data, reply);
+    if (ret == DM_IPC_NOT_REGISTER_FUNC) {
+        LOGW("unsupport code: %d", code);
+        return IPCObjectStub::OnRemoteRequest(code, data, reply, option);
+    }
+    return ret;
+}
+
+int32_t IpcServerStub::SendCmd(int32_t cmdCode, std::shared_ptr<IpcReq> req, std::shared_ptr<IpcRsp> rsp)
+{
+    return DM_OK;
+}
+
+ServiceRunningState IpcServerStub::QueryServiceState() const
+{
+    return state_;
+}
+
+int32_t IpcServerStub::RegisterDeviceManagerListener(std::string &pkgName, sptr<IRemoteObject> listener)
+{
+    if (pkgName.empty() || listener == nullptr) {
+        LOGE("Error: parameter invalid");
+        return DM_POINT_NULL;
+    }
+    LOGI("In, pkgName: %s", pkgName.c_str());
+    std::lock_guard<std::mutex> autoLock(listenerLock_);
+    auto iter = dmListener_.find(pkgName);
+    if (iter != dmListener_.end()) {
+        LOGI("RegisterDeviceManagerListener: listener already exists");
+        return DM_OK;
+    }
+    sptr<AppDeathRecipient> appRecipient = sptr<AppDeathRecipient>(new AppDeathRecipient());
+    if (!listener->AddDeathRecipient(appRecipient)) {
+        LOGE("RegisterDeviceManagerListener: AddDeathRecipient Failed");
+    }
+    dmListener_[pkgName] = listener;
+    appRecipient_[pkgName] = appRecipient;
+    return DM_OK;
+}
+
+int32_t IpcServerStub::UnRegisterDeviceManagerListener(std::string &pkgName)
+{
+    if (pkgName.empty()) {
+        LOGE("Error: parameter invalid");
+        return DM_POINT_NULL;
+    }
+    LOGI("In, pkgName: %s", pkgName.c_str());
+    std::lock_guard<std::mutex> autoLock(listenerLock_);
+    auto listenerIter = dmListener_.find(pkgName);
+    if (listenerIter == dmListener_.end()) {
+        LOGI("UnRegisterDeviceManagerListener: listener not exists");
+        return DM_OK;
+    }
+    auto recipientIter = appRecipient_.find(pkgName);
+    if (recipientIter == appRecipient_.end()) {
+        LOGI("UnRegisterDeviceManagerListener: appRecipient not exists");
+        dmListener_.erase(pkgName);
+        return DM_OK;
+    }
+    auto listener = listenerIter->second;
+    auto appRecipient = recipientIter->second;
+    listener->RemoveDeathRecipient(appRecipient);
+    appRecipient_.erase(pkgName);
+    dmListener_.erase(pkgName);
+    return DM_OK;
+}
+
+const std::map<std::string, sptr<IRemoteObject>> &IpcServerStub::GetDmListener()
+{
+    return dmListener_;
+}
+
+const sptr<IpcRemoteBroker> IpcServerStub::GetDmListener(std::string pkgName) const
+{
+    auto iter = dmListener_.find(pkgName);
+    if (iter == dmListener_.end()) {
+        return nullptr;
+    }
+    auto remote = iter->second;
+    sptr<IpcRemoteBroker> dmListener = iface_cast<IpcRemoteBroker>(remote);
+    return dmListener;
+}
+
+void AppDeathRecipient::OnRemoteDied(const wptr<IRemoteObject> &remote)
+{
+    LOGW("AppDeathRecipient: OnRemoteDied");
+    std::map<std::string, sptr<IRemoteObject>> listeners = IpcServerStub::GetInstance().GetDmListener();
+    std::string pkgName;
+    for (auto iter : listeners) {
+        if (iter.second == remote.promote()) {
+            pkgName = iter.first;
+            break;
+        }
+    }
+    if (pkgName.empty()) {
+        LOGE("AppDeathRecipient: OnRemoteDied, no pkgName matched");
+        return;
+    }
+    LOGI("AppDeathRecipient: OnRemoteDied for %s", pkgName.c_str());
+    IpcServerStub::GetInstance().UnRegisterDeviceManagerListener(pkgName);
+}
+} // namespace DistributedHardware
+} // namespace OHOS
